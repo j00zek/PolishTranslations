c2ec053853e16bdbce769505840bb400e1042334
j00zek <j00zek@ubuntu.(none)>
2016-01-04 11:53:21 +0100

    minor update

---
+++ build_ipk.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+myDir=`dirname $0`
+PluginName='TranslationsUpdater'
+PluginPath="/usr/lib/enigma2/python/Plugins/Extensions/$PluginName"
+PluginGITpath="$myDir/$PluginName"
+wersja=`grep 'wersja=' <$myDir/$PluginName/__init__.py|cut -d '=' -f2`
+
+$myDir/../IPKs-storage/build_ipk_package.sh "$PluginName" "$PluginPath" "$PluginGITpath" "$wersja" "enigma2-plugin-extensions"
+
+if ! `grep -q "$myDir/build_ipk.sh" <$myDir/../IPKs-storage/build_ipk_packages.sh`;then
+  echo "$myDir/build_ipk.sh">>$myDir/../IPKs-storage/build_ipk_packages.sh
+fi
+exit 0
